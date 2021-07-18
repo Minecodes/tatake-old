89c6d105578c3b790d496e4e50907d1529dea8bf
Minecodes <65566012+Minecodes@users.noreply.github.com>
2021-07-18 18:31:44 +0200

    Delete test.ts

--- src/commands/test.ts
+++
@@ -1,9 +0,0 @@
-import { MessageButton } from "discord-buttons"
-
-export default async (message, args, client) => {
-    let button = new MessageButton()
-        .setLabel("Hi")
-        .setStyle("url")
-        .setURL("https://www.thisworldthesedays.com/bookhead.html");
-    message.channel.send("Hi 👋🏻", button);
-}
